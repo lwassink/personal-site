b5b99bd73024819401cf591d3867a77beaea0f39
Luke Wassink <lwassink@gmail.com>
2017-05-01 09:40:51 -0700

    refactor api-util

--- frontend/src/util/api_util.js
+++ frontend/src/util/api_util.js
@@ -1,3 +1,5 @@
-export const requestPosts = () => fetch('http://localhost:8000/api/posts', {mode: 'cors'});
+const domain = 'http://localhost:8000';
 
-export const requestProjects = () => fetch('http://localhost:8000/api/projects', {mode: 'cors'});
+export const requestPosts = () => fetch(`${domain}/api/posts`, {mode: 'cors'});
+
+export const requestProjects = () => fetch(`${domain}/api/projects`, {mode: 'cors'});
