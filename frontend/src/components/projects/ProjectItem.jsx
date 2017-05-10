25b99afd59ee5d6c59b15df5c9258ff3d8a5dea7
Luke Wassink <lwassink@gmail.com>
2017-05-10 14:16:50 -0700

    refactor expand/colapse

--- frontend/src/components/projects/ProjectItem.jsx
+++ frontend/src/components/projects/ProjectItem.jsx
@@ -1,85 +1,23 @@
 import React from 'react';
+import { Collapse } from 'react-collapse';
 
 class ProjectItem extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      open: this.props.open,
-      phase: null
+      open: this.props.open
     };
     this.toggleOpen = this.toggleOpen.bind(this);
   }
 
-  componentDidMount() {
-    if (this.state.open)
-      this.setState({
-        height: document.getElementById(this.props.project.id).offsetHeight
-      });
-  }
-
-  isMeasuring() {
-    return this.state.phase === "measuring";
-  }
-
-  componentDidUpdate() {
-    switch (this.state.phase) {
-    case null:
-      break;
-    case "measuring":
-      window.setTimeout(() => this.setState({
-        height: document.getElementById(this.props.project.id).offsetHeight,
-        phase: "startingOpen"
-      }), 0);
-      break;
-    case "startingOpen":
-      window.setTimeout(() => this.setState({
-        phase: "finishingOpen"
-      }), 20);
-      break;
-    case "finishingOpen":
-      window.setTimeout(() => this.setState({
-        phase: null,
-        open: true
-      }), 600);
-      break;
-    case "closing":
-      window.setTimeout(() => this.setState({
-        phase: null,
-        open: false
-      }), 0);
-      break;
-    }
-  }
-
-  open() {
-    this.setState({ phase: "measuring" });
-  }
-
-  close() {
-    this.setState({ phase: "closing" });
-  }
-
   toggleOpen() {
-    if (this.state.open) { this.close(); }
-    else { this.open(); }
+    this.setState({ open: !this.state.open })
   }
 
   render() {
     const { project } = this.props;
-    const rotateClass = (this.state.phase === "finishingOpen" || this.state.open) ? "rotate" : "";
-    const measureClass = this.isMeasuring() ? "measure" : "";
-
-    let height;
-    if (this.state.phase === "finishingOpen" ||
-       this.state.phase === "closing") {
-     height = this.state.height;
-    } else if (this.isMeasuring() || this.state.open) {
-      height = "auto";
-    } else {
-      height = 0;
-    }
-    const sectionStyle = { height };
+    const rotateClass = this.state.open ? "rotate" : "";
 
     return (
       <li className="index-item">
@@ -89,15 +27,12 @@ class ProjectItem extends React.Component {
           {project.title}
         </h1>
         <main>
-          <section
-            style={sectionStyle}
-            className={measureClass}
-            id={project.id} >
+          <Collapse isOpened={this.state.open} >
             <p> {project.description} </p>
             <p>
               <label>technologies used:</label> {project.technologies}
             </p>
-          </section>
+          </Collapse>
           <p>
             {project.site_url ?<span>
               <label>live site:</label>
