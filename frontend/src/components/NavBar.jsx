a2004be3aeed5bdf1c3427fc5bd3fa53a1fde3b7
Luke Wassink <lwassink@gmail.com>
2017-05-20 22:02:31 -0700

    close nav on blur

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -6,12 +6,34 @@ import Logo from './Logo';
 class NavBar extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { open: false };
+    this.state = {
+      open: false,
+      width: window.innerWidth
+    };
     this.toggleOpen = this.toggleOpen.bind(this);
+    this.checkWidth = this.checkWidth.bind(this);
+    this.close = this.close.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.checkWidth);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.checkWidth);
+  }
+
+  checkWidth() {
+    if (window.innerWidth > 550 && this.state.width <= 550) this.close();
+    this.setState({ width: window.innerWidth });
   }
 
   toggleOpen() {
     this.setState({ open: !this.state.open });
+  }
+
+  close() {
+    this.setState({ open: false });
   }
 
   render () {
@@ -19,7 +41,7 @@ class NavBar extends React.Component {
     const navClass = this.state.open ? "nav-bar open" : "nav-bar";
 
     return (
-      <div className={navClass}>
+      <div className={navClass} tabIndex="0" onBlur={this.close}>
         <section className='center'>
           <section className='hamburger'>
             <i className="fa fa-bars" aria-hidden="true"
