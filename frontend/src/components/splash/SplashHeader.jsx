68b7a4400cee6486609ff1622f9eb073781dad2d
Luke Wassink <lwassink@gmail.com>
2017-06-22 20:32:15 -0700

    update resume

--- frontend/src/components/splash/SplashHeader.jsx
+++ frontend/src/components/splash/SplashHeader.jsx
@@ -6,7 +6,7 @@ export default () => (
   <div className="splash-section splash-header">
     <img src="/assets/images/luke-wassink-headshot.jpg" />
     <h1>Luke&nbsp;Wassink</h1>
-    <h3>Software developer</h3>
+    <h3>Software developer, Ph.D. Mathematics</h3>
     <PDFTag
     label="R&eacute;sum&eacute;"
     url="/assets/luke-wassink-resume.pdf"
